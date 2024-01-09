9fe5eed5cfa773f17bcfde344d838963f9008b37
siddharth7020 <sidh1583@gmail.com>
2024-01-09 16:44:38 +0530

    3 module

--- 01-starting-project/01-starting-project/src/App.jsx
+++ 01-starting-project/01-starting-project/src/App.jsx
@@ -2,17 +2,34 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from './Components/Header/Header.jsx'
 import CoreComponent from './Components/CoreConcept.jsx'
 import TabButton from './Components/TabButton.jsx';
+import { useState } from 'react';
+import { EXAMPLES } from './data.js'
 
 function App() {
 
-  let tabContent = 'Plese tab the button'
+  const [selectedTopic, setSelectedTopic] = useState();
 
- function handleSelect (selectedButton) {
-  // selectedButton is a props to click the component , jsx, props, state button
-  tabContent = selectedButton;
-  console.log(tabContent);
- }
- console.log('app content');
+  function handleSelect(selectedButton) {
+    // selectedButton is a props to click the component , jsx, props, state button
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
+  }
+  console.log('app content');
+  // approch 2
+  let tabContent = <p>Please select topic</p>
+  if (selectedTopic) {
+    tabContent = <div id='tab-content'>
+      <h3>
+        {EXAMPLES[selectedTopic].title}
+      </h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[selectedTopic].code}
+        </code>
+      </pre>
+    </div>
+  }
 
   return (
     <div>
@@ -32,12 +49,28 @@ function App() {
             Examples
           </h2>
           <menu>
-            <TabButton onSelet={() =>handleSelect('components')}>Components</TabButton>
-            <TabButton onSelet={() =>handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelet={() =>handleSelect('props')}>Props</TabButton>
-            <TabButton onSelet={() =>handleSelect('state')}>State</TabButton>
+            <TabButton onSelet={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelet={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelet={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelet={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {/* Approch 1
+          {!selectedTopic && (<p>Please select topic</p>)}
+          {selectedTopic && (
+            <div id='tab-content'>
+              <h3>
+                {EXAMPLES[selectedTopic].title}
+              </h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
+          )} */ }
           {tabContent}
+
         </section>
       </main>
     </div>
--- 01-starting-project/01-starting-project/src/Components/TabButton.jsx
+++ 01-starting-project/01-starting-project/src/Components/TabButton.jsx
@@ -1,5 +1,5 @@
 export default function TabButton ({children,onSelet}) {
-    console.log('tab content');
+    // console.log('tab content');
     return(
         <li><button onClick={onSelet}>{children}</button></li>
     );
